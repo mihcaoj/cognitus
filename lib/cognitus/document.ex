2cc7096a9e79af12b76b8385377cc99c80c58471
Séverine Oppliger <severine.oppliger@unifr.ch>
2024-11-13 20:30:55 +0100

    Start implementation of CRDT

--- lib/cognitus/document.ex
+++ lib/cognitus/document.ex
@@ -1,4 +1,29 @@
+@doc '''
+Document is structured as a `AWLWWMAP` where keys are character's id
+and values are tuples {character, position}
+
+id is a tuple {process_nr, insertion_nr}
+'''
 defmodule Cognitus.Document do
+  @defstruct id = {Integer, Integer}
+
+  # Insertion counter
+  counter = 0
+
+  # Start an OR-Set CRDT.
+  {:ok, document_text} = DeltaCrdt.start_link(DeltaCrdt.AWLWWMap)
+
+  def insert(crdt_pid, id, char, position) do
+    key = {id}  # Using a tuple to ensure unique keys
+    value = {char, position}
+    DeltaCrdt.mutate(crdt_pid, :add, [key, value])
+  end
+
+  def getTextContent(crdt_pid) do
+    positions = document_text.keys()
+  end
+  '''
+  OLD version
   defstruct content: [] # Simple list placeholder for the future CRDT implementation
 
   def insert(%__MODULE__{content: content} = document, id, char, position) do
@@ -11,4 +36,6 @@ defmodule Cognitus.Document do
     new_content = Enum.reject(content, fn item -> item.id == id end)
     %__MODULE__{document | content: new_content}
   end
+
+  '''
 end
