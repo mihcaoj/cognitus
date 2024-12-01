3492cf77820da50c225e7b768607a30518c04bb8
Séverine Oppliger <severine.oppliger@unifr.ch>
2024-12-01 16:37:32 +0100

    Comment files to be deleted

--- lib/cognitus_web/controllers/page_controller.ex
+++ lib/cognitus_web/controllers/page_controller.ex
@@ -1,11 +1,11 @@
-defmodule CognitusWeb.PageController do
-  use CognitusWeb, :controller
+#defmodule CognitusWeb.PageController do
+#  use CognitusWeb, :controller
 
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
+#  def home(conn, _params) do
+#    # The home page is often custom made,
+#    # so skip the default app layout.
+#    render(conn, :home, layout: false)
+#  end
+#end
 
 # TODO: delete because we use LiveView instead of controllers
